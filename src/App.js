a69368174f32506a465a58d1f0a34e7f7ee9e89c
achuworks <achuworks2005@gmail.com>
2025-03-07 12:25:50 +0530

    Added map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-// import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sideebar from './components/Sidebar';
+import { Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
 import Map from './components/Map';
+// Import other components for the different routes you're defining
+
 function App() {
   return (
-    <>
-      <Sideebar />
-      <div style={{ marginLeft: '250px', padding: '20px' }}>
+    <div style={{ display: 'flex' }}>
+      <Sidebar />
+      <div style={{ flex: 1, padding: '20px' }}>
         <Routes>
           <Route path="/hotspot" element={<Map />} />
+          
         </Routes>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,13 @@
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const Map = () => {
-  return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '1000px', width: '100%' }}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-    </MapContainer>
-  );
-};
-
-export default Map;
+const LeafletMap = () => { 
+    return (
+      <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '1000px', width: '100%' }}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </MapContainer>
+    );
+  };
+  
+  export default LeafletMap;
+  
