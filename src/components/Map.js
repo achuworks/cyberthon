3561cadd0442a4c3c5a47b0dc6c45a86f8293964
achuworks <achuworks2005@gmail.com>
2025-03-11 13:53:18 +0530

    new changes

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,16 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-const LeafletMap = () => { 
-    return (
-      <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '1000px', width: '100%' }}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      </MapContainer>
-    );
-  };
-  
-  export default LeafletMap;
-  
+const LeafletMap = () => {
+  return (
+    <MapContainer
+      center={[11.1271, 78.6569]} // Tamil Nadu coordinates
+      zoom={7} // Adjust zoom level to fit Tamil Nadu
+      style={{ height: "850px", width: "100%" }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    </MapContainer>
+  );
+};
+
+export default LeafletMap;
